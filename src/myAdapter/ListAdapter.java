407609e9865e67162ecb0ca7c30bbbb8573a0935
FengDiesel <dalbiancoluca04@gmail.com>
2025-06-02 23:44:48 +0200

    Test JUnit TDD

--- src/myAdapter/ListAdapter.java
+++ src/myAdapter/ListAdapter.java
@@ -20,7 +20,7 @@ public class ListAdapter implements HList, HCollection {
     }
 
     // HCollection
-    
+
     @Override
     public int size() {
         return delegate.size();
@@ -31,82 +31,224 @@ public class ListAdapter implements HList, HCollection {
         return delegate.size() == 0;
     }
 
-
+    @Override
     public boolean contains(Object o) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return delegate.contains(o);
     }
-
+    
+    //provvisorio
+    @Override
     public HIterator iterator() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return new IteratorAdapter();
     }
 
+    private class IteratorAdapter implements HIterator {
+        private int cursor = 0;
+        private boolean canRemove = false;
+
+        @Override
+        public boolean hasNext() {
+            return cursor < delegate.size();
+        }
+
+        @Override
+        public Object next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+            Object next = delegate.elementAt(cursor);
+            cursor++;
+            canRemove = true;
+            return next;
+        }
+
+        @Override
+        public void remove() {
+            if (!canRemove) {
+                throw new IllegalStateException();
+            }
+            delegate.removeElementAt(--cursor);
+            canRemove = false;
+        }
+    }
+    ///////////////////////////
+
+
+    @Override
     public Object[] toArray() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        Object[] arr = new Object[delegate.size()];
+        for (int i = 0; i < delegate.size(); i++) {
+            arr[i] = delegate.elementAt(i);
+        }
+        return arr;
     }
 
+    @Override
     public Object[] toArray(Object[] a) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        int size = delegate.size();
+        if (a.length < size) {
+            // crea un nuovo array dello stesso tipo con la dimensione giusta
+            a = (Object[]) java.lang.reflect.Array.newInstance(
+                a.getClass().getComponentType(), size
+            );
+        }
+        for (int i = 0; i < size; i++) {
+            a[i] = delegate.elementAt(i);
+        }
+        if (a.length > size) {
+            a[size] = null; // terminatore
+        }
+        return a;
     }
 
+    @Override
     public boolean add(Object o) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        delegate.addElement(o);  // Vector.addElement()
+        return true;
     }
 
+
+    @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return delegate.removeElement(o); // rimuove solo la prima occorrenza
     }
 
+    @Override
     public boolean containsAll(HCollection c) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        HIterator it = c.iterator();
+        while (it.hasNext()) {
+            if (!delegate.contains(it.next())) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    @Override
     public boolean addAll(HCollection c) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        boolean modified = false;
+        HIterator it = c.iterator();
+        while (it.hasNext()) {
+            delegate.addElement(it.next());
+            modified = true;
+        }
+        return modified;
     }
 
+    @Override
     public boolean removeAll(HCollection c) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        boolean modified = false;
+        HIterator it = c.iterator();
+        while (it.hasNext()) {
+            Object elem = it.next();
+            while (delegate.contains(elem)) {
+                delegate.removeElement(elem);
+                modified = true;
+            }
+        }
+        return modified;
     }
 
+    @Override
     public boolean retainAll(HCollection c) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        boolean modified = false;
+        int i = 0;
+        while (i < delegate.size()) {
+            Object elem = delegate.elementAt(i);
+            if (!c.contains(elem)) {
+                delegate.removeElementAt(i);
+                modified = true;
+            } else {
+                i++;
+            }
+        }
+        return modified;
     }
 
+    @Override
     public void clear() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        delegate.removeAllElements();
     }
 
+    @Override
     public boolean equals(Object o) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (o == this) return true;
+        if (!(o instanceof HList)) return false;
+        HList other = (HList) o;
+        if (size() != other.size()) return false;
+
+        for (int i = 0; i < size(); i++) {
+            Object e1 = get(i);
+            Object e2 = other.get(i);
+            if (e1 == null ? e2 != null : !e1.equals(e2)) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    @Override
     public int hashCode() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        int hash = 1;
+        for (int i = 0; i < size(); i++) {
+            Object obj = get(i);
+            hash = 31 * hash + (obj == null ? 0 : obj.hashCode());
+        }
+        return hash;
     }
 
     // HList
+    @Override
     public Object get(int index) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
+        if (index < 0 || index >= delegate.size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + delegate.size());
+        }
+    return delegate.elementAt(index);
+}
+
 
     public Object set(int index, Object element) {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
+    @Override
     public void add(int index, Object element) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (index < 0 || index > delegate.size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + delegate.size());
+        }
+        delegate.insertElementAt(element, index);
     }
 
+    @Override
     public Object remove(int index) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (index < 0 || index >= delegate.size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + delegate.size());
+        }
+        Object old = delegate.elementAt(index);
+        delegate.removeElementAt(index);
+        return old;
     }
 
+    @Override
     public int indexOf(Object o) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        for (int i = 0; i < delegate.size(); i++) {
+            Object e = delegate.elementAt(i);
+            if (e == null ? o == null : e.equals(o)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
+    @Override
     public int lastIndexOf(Object o) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        for (int i = delegate.size() - 1; i >= 0; i--) {
+            Object e = delegate.elementAt(i);
+            if (e == null ? o == null : e.equals(o)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public HListIterator listIterator() {
--- test/myTest/ListAdapterTest.java
+++ test/myTest/ListAdapterTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import myAdapter.HCollection;
 import myAdapter.HList;
 import myAdapter.ListAdapter;
 
@@ -23,4 +24,226 @@ public class ListAdapterTest {
         assertEquals("Una nuova lista dovrebbe avere size 0", 0, list.size());
         assertTrue("Una nuova lista dovrebbe essere vuota", list.isEmpty());
     }
+
+    //
+
+    @Test
+    public void testAddAndGet() {
+        assertTrue("add() deve restituire true", list.add("A"));
+        assertTrue(list.add("B"));
+        assertEquals("Dopo due inserti, size() deve restituire 2", 2, list.size());
+        assertEquals("get(0) deve restituire il primo elemento", "A", list.get(0));
+        assertEquals("get(1) deve restituire il secondo elemento", "B", list.get(1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNegativeIndex() {
+        list.get(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetIndexOutOfBounds() {
+        list.add("X");
+        list.get(1); // index >= size
+    }
+
+    //
+
+    @Test
+    public void testAddAtIndex() {
+        list.add("A"); // index 0
+        list.add("C"); // index 1
+        list.add(1, "B"); // inserisce B tra A e C
+        assertEquals(3, list.size());
+        assertEquals("A", list.get(0));
+        assertEquals("B", list.get(1));
+        assertEquals("C", list.get(2));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddAtNegativeIndex() {
+        list.add(-1, "X");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddAtTooLargeIndex() {
+        list.add("A");
+        list.add(2, "B"); // posizione 2 non esiste (size = 1)
+    }
+
+    //
+
+    @Test
+    public void testRemoveByIndex() {
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        Object removed = list.remove(1); // rimuove "B"
+        assertEquals("B", removed);
+        assertEquals(2, list.size());
+        assertEquals("A", list.get(0));
+        assertEquals("C", list.get(1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveIndexOutOfBounds() {
+        list.add("X");
+        list.remove(1); // posizione 1 non esiste
+    }
+
+    @Test
+    public void testRemoveByObject() {
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        assertTrue(list.remove("B"));
+        assertEquals(2, list.size());
+        assertEquals("A", list.get(0));
+        assertEquals("C", list.get(1));
+
+        assertFalse("Rimuovere un elemento non presente deve restituire false", list.remove("Z"));
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void testIndexOfAndLastIndexOf() {
+        list.add("A");
+        list.add("B");
+        list.add("A");
+        list.add("C");
+        assertEquals(0, list.indexOf("A"));
+        assertEquals(2, list.lastIndexOf("A"));
+        assertEquals(1, list.indexOf("B"));
+        assertEquals(1, list.lastIndexOf("B"));
+        assertEquals(-1, list.indexOf("Z"));
+    }
+
+    @Test
+    public void testContainsAndContainsAll() {
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        assertTrue(list.contains("B"));
+        assertFalse(list.contains("Z"));
+
+        HCollection other = new ListAdapter();
+        other.add("A");
+        other.add("C");
+        assertTrue(list.containsAll(other));
+
+        other.add("Z");
+        assertFalse(list.containsAll(other));
+    }
+
+    @Test
+    public void testAddAllRemoveAllRetainAll() {
+        HCollection other = new ListAdapter();
+        other.add("X");
+        other.add("Y");
+        other.add("Z");
+
+        // Test addAll
+        assertTrue(list.addAll(other));
+        assertEquals(3, list.size());
+        assertEquals("X", list.get(0));
+        assertEquals("Y", list.get(1));
+        assertEquals("Z", list.get(2));
+
+        // Test removeAll
+        list.add("A");
+        list.add("B");
+        HCollection toRemove = new ListAdapter();
+        toRemove.add("X");
+        toRemove.add("B");
+
+        assertTrue(list.removeAll(toRemove));
+        assertFalse(list.contains("X"));
+        assertFalse(list.contains("B"));
+        assertTrue(list.contains("Y"));
+        assertTrue(list.contains("Z"));
+
+        // Test retainAll
+        list.clear();
+        list.add("X");
+        list.add("Y");
+        list.add("Z");
+        list.add("A");
+
+        HCollection toRetain = new ListAdapter();
+        toRetain.add("Z");
+        toRetain.add("A");
+
+        assertTrue(list.retainAll(toRetain));
+        assertEquals(2, list.size());
+        assertTrue(list.contains("Z"));
+        assertTrue(list.contains("A"));
+        assertFalse(list.contains("X"));
+        assertFalse(list.contains("Y"));
+
+    }
+
+    @Test
+    public void testClear() {
+        list.add("A");
+        list.add("B");
+        assertEquals(2, list.size());
+        list.clear();
+        assertEquals(0, list.size());
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void testEqualsAndHashCode() {
+        HList list1 = new ListAdapter();
+        HList list2 = new ListAdapter();
+
+        list1.add("A");
+        list1.add("B");
+
+        list2.add("A");
+        list2.add("B");
+
+        assertTrue("Liste con stessi elementi devono essere uguali", list1.equals(list2));
+        assertEquals("Liste uguali devono avere stesso hashCode", list1.hashCode(), list2.hashCode());
+
+        list2.add("C");
+        assertFalse("Liste con elementi diversi non sono uguali", list1.equals(list2));
+    }
+
+    @Test
+    public void testToArray() {
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        Object[] arr = list.toArray();
+        assertEquals(3, arr.length);
+        assertEquals("A", arr[0]);
+        assertEquals("B", arr[1]);
+        assertEquals("C", arr[2]);
+    }
+
+    @Test
+    public void testToArrayWithParameter() {
+        list.add("X");
+        list.add("Y");
+
+        // Caso: array passato abbastanza grande
+        String[] arr1 = new String[3];
+        Object[] result1 = list.toArray(arr1);
+        assertSame("Deve restituire l'array passato se sufficientemente grande", arr1, result1);
+        assertEquals("X", arr1[0]);
+        assertEquals("Y", arr1[1]);
+        assertNull("L'elemento dopo gli elementi deve essere null", arr1[2]);
+
+        // Caso: array passato troppo piccolo → restituito uno nuovo
+        String[] arr2 = new String[1];
+        Object[] result2 = list.toArray(arr2);
+        assertNotSame("Deve restituire un nuovo array se troppo piccolo", arr2, result2);
+        assertEquals("X", result2[0]);
+        assertEquals("Y", result2[1]);
+    }
+
+
 }
