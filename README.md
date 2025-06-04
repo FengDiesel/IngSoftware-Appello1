d63abc446e8e525d08a114737c53ae44b52ff069
FengDiesel <dalbiancoluca04@gmail.com>
2025-06-04 23:49:29 +0200

    Fix generali

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # IngSoftware-Appello1
+Studente: Luca Dal Bianco, Matricola 2102603
+
+Cartelle:
+- src/myAdapter: codice sorgente
+- test/myTest: test JUnit
+- doc/javadoc: Javadoc HTML
+- doc/Documentation.pdf: documentazione test
+
+Per eseguire i test:
+javac -cp .:junit-4.13.2.jar test/myTest/TestRunner.java
+java -cp .:junit-4.13.2.jar test.myTest.TestRunner
--- test/myTest/TestRunner.java
+++ test/myTest/TestRunner.java
@@ -5,30 +5,27 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 /**
- * Classe di lancio dei test JUnit.
+ * Classe di lancio dei test JUnit per il progetto.
+ * Esegue tutti i test definiti nella suite.
  */
 public class TestRunner {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(
-    ListAdapterTest.class
+            ListAdapterTest.class
         );
 
-        System.out.println("Numero test eseguiti: " + result.getRunCount());
-        System.out.println("Fallimenti: " + result.getFailureCount());
+        System.out.println("=== Risultati Test ===");
+        System.out.println("Totale test eseguiti: " + result.getRunCount());
+        System.out.println("Numero di fallimenti: " + result.getFailureCount());
 
         if (!result.wasSuccessful()) {
-            System.out.println("Dettagli fallimenti:");
+            System.out.println("--- Dettagli fallimenti ---");
             for (Failure failure : result.getFailures()) {
                 System.out.println(failure.toString());
+                failure.getException().printStackTrace();
             }
         } else {
-            System.out.println("Tutti i test sono passati.");
+            System.out.println("Tutti i test sono stati superati con successo.");
         }
-
-        for (Failure failure : result.getFailures()) {
-    System.out.println(failure.toString());
-    failure.getException().printStackTrace(); // <--- AGGIUNGI QUESTO
-}
-
     }
 }
